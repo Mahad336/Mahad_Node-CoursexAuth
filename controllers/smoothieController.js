0e8b76b10d0bad7a78afa2219485e36b4145d722
Mahad366 <mahad.younis@gigalabs.co>
2022-12-15 16:45:16 +0500

    almost done !!!

--- controllers/smoothieController.js
+++ controllers/smoothieController.js
@@ -1,7 +1,7 @@
 const Smoothie = require("../models/smoothie");
 
 module.exports.createSmoothie_get = (req, res) => {
-  res.render("createSmoothie");
+  res.render("createSmoothie", { url: req.url });
 };
 
 module.exports.createSmoothie_post = (req, res) => {
@@ -33,6 +33,50 @@ module.exports.myRecipes_get = (req, res) => {
     .sort({ createdAt: -1 })
     .then((result) => {
       res.render("myRecipes", { smoothies: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports.smoothie_details = (req, res) => {
+  const id = req.params.id;
+  Smoothie.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports.smoothie_delete = (req, res) => {
+  const id = req.params.id;
+  Smoothie.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/allRecipes" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports.smoothie_edit_get = (req, res) => {
+  const id = req.params.id;
+  res.render("createSmoothie", { url: req.url });
+  // Smoothie.findByIdAndDelete(id)
+  //   .then((result) => {
+  //     res.json({ redirect: "/allRecipes" });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  // });
+};
+module.exports.smoothie_edit_put = (req, res) => {
+  const id = req.params.id;
+  Smoothie.findByIdAndUpdate(id, req.body, { new: true })
+    .then((result) => {
+      res.json({ redirect: "/allRecipes" });
     })
     .catch((err) => {
       console.log(err);
--- routes/smoothieRoutes.js
+++ routes/smoothieRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const smoothieController = require("../controllers/smoothieController");
+const { requireAuth } = require("../middleware/authMiddleware");
 
 const srouter = express.Router();
 
 srouter.get("/createSmoothie", smoothieController.createSmoothie_get);
-srouter.get("/allRecipes", smoothieController.allRecipes_get);
+srouter.get("/allRecipes", requireAuth, smoothieController.allRecipes_get);
 srouter.get("/myRecipes", smoothieController.myRecipes_get);
+srouter.get("/allRecipes/:id", smoothieController.smoothie_details);
+srouter.delete("/allRecipes/:id", smoothieController.smoothie_delete);
+srouter.get("/allRecipes/:id/edit", smoothieController.smoothie_edit_get);
+srouter.put("/allRecipes/:id/edit", smoothieController.smoothie_edit_put);
 srouter.post("/createSmoothie", smoothieController.createSmoothie_post);
 
 module.exports = srouter;
